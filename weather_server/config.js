98f54f51be098639d73f521beb7e7e937b0af8e5
azare242 <azare242@gmail.com>
2023-11-23 22:46:40 +0330

    im a idiot :D

--- weather_server/config.js
+++ weather_server/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 9999,
     REDIS_HOST: process.env.REDIS_HOST || "localhost",
-    REDIS_PORT: process.env.exports || 6379,
-    REDIS_PAIR_TTL: process.env.REDIS_PAIR_TTL || undefined,
-    WEATHER_URL: process.env.WEATHER_URL || undefined,
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
+    REDIS_PAIR_TTL: process.env.REDIS_PAIR_TTL || 20,
+    WEATHER_URL: process.env.WEATHER_URL,
     WEATHER_AUTH: process.env.WEATHER_AUTH || undefined
 }
